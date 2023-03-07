d43cb86648763e9a921cefad4e0422b94f6190c1
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-03-07 15:13:38 +0600

    header top

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,94 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
 
 const Header = () => {
   return (
-    <>
-      <header className="header-top-strip py-3">
-        <div className="container--xxl">
+    <header>
+      <div className="header-top-strip py-3">
+        <div className="container-xxl">
           <div className="row">
-            <div className="col-6"></div>
-            <div className="col-6"> </div>
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Shipping Over $100 & Free Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:{" "}
+                <a className="text-white" href="tel:+16454244">
+                  +1645252545
+                </a>
+              </p>
+            </div>
           </div>
         </div>
-      </header>
-    </>
+      </div>
+      <div className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h1>
+                <Link className="text-white" to="/">
+                  Digitic
+                </Link>
+              </h1>
+            </div>
+            <div className="col-5">
+              <div class="input-group">
+                <input
+                  type="search"
+                  class="form-control"
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
+                  aria-describedby="search-product"
+                />
+                <span class="input-group-text p-3" id="search-product">
+                  <BsSearch className="fs-6" />
+                </span>
+              </div>
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/compare.svg" alt="compare" />
+                    <p className="mb-0">
+                      Compare <br /> Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/wishlist.svg" alt="wishlist" />
+                    <p className="mb-0">
+                      Favourite <br /> Wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/user.svg" alt="user" />
+                    <p className="mb-0">
+                      Log In <br /> My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className="de-flex flex-column">
+                      <span className="badge bg-white text-dark">0</span>
+                    </div>
+                    <p className="mb-0">$0.00</p>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
 
