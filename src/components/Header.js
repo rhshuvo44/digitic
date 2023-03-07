ae1da0dc610c46d33252eeaeb9d200e9c09d9fe4
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-03-07 10:36:41 +0600

    project ready

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container--xxl">
+          <div className="row">
+            <div className="col-6"></div>
+            <div className="col-6"> </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
