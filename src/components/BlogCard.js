b91771b995dd57da222225684388fa6a77c1fa72
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-05-10 14:44:15 +0600

    blog section

---
+++ src/components/BlogCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const BlogCard = () => {
+  return (
+    <div className="col-3">
+      <div className="blog-card">
+        <div className="card-image">
+          <img src="images/blog-1.jpg" className="img-fluid" alt="" />
+        </div>
+        <div className="blog-content">
+          <p className="date">1 Dec, 2022</p>
+          <h5 className="title">Lorem ipsum dolor sit amet.</h5>
+          <p className="desc">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis,
+            deleniti.
+          </p>
+          <Link to="/" className="button">
+            Read More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
+import BlogCard from "../components/BlogCard";
 const Home = () => {
   return (
     <>
@@ -188,7 +189,7 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="marquee py-5">
+      <section className="marquee py-5 home-wrraper-2">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
@@ -221,6 +222,21 @@ const Home = () => {
                 </Marquee>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="blog-wrraper py-5 home-wrraper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <h2 className="section-heading">
+                Our Leates Blog
+              </h2>
+            </div>
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
+            <BlogCard />
           </div>
         </div>
       </section>
